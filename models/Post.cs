e6de00ad6689d53772b3ee4b3d957162f57444ff
timnguyen90 <33167258+timnguyen90@users.noreply.github.com>
2018-07-15 22:20:05 +0700

    Add constructor to Post class

--- models/Post.cs
+++ models/Post.cs
@@ -1,5 +1,13 @@
 public class Post{
 	public string Id { get; set; }
-	public string Title { get; set; }\
+	public string Title { get; set; }
 	public string Body { get; set; }
+
+	public Post(){}
+	public Post(string Id, string Title, string Body){
+		this.Id = Id;
+		this.Title = Title;
+		this.Body = Body;
+	}
+	
 }
